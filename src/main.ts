49eb5bcfaaefc462c128b4ca3f2775c74a244485
MiguelDreis <mil1704@hotmail.com>
2022-11-26 21:36:02 -0300

    Iniciando repositorio

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'normalize.css';
+import '@/assets/main.css'
+
+createApp(App).mount('#app');
